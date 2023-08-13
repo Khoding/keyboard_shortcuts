85845da49e82d68506f806173f0c234d51e7ac39
Khoding <julien.rutscho@ceff.ch>
2023-08-13 18:23:28 +0200

    markdown for how tos

--- keyboard_shortcuts/settings/project/installed_apps.py
+++ keyboard_shortcuts/settings/project/installed_apps.py
@@ -22,7 +22,8 @@ INSTALLED_APPS = [
     "tailwind",
     "heroicons",
     "theme",
-    # applications
-    "shortcuts",
+    # Package Apps
     "rest_framework",
+    # Apps
+    "shortcuts",
 ]
